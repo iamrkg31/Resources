47aaee83598cad0c4b13ec751c45ef7ff1cf9e41
iamrkg31 <rahul199431@gmail.com>
2018-09-28 14:58:20 +0530

    read_write_csv_pandas.py

--- python/read_write_csv_pandas.py
+++ python/read_write_csv_pandas.py
@@ -4,23 +4,22 @@ import pandas as pd
 from nltk.tokenize import sent_tokenize
 
 # Import data
-df1 = pd.read_csv("/home/rahul/train.csv")
-checks = pd.read_csv("/home/rahul/check_list.csv")["checks"].tolist()
-checks_regex = "("+'|'.join(checks)+")"
+df1 = pd.read_csv("/home/rahul/sentiment_training_data.csv")
 
-def tokenize_to_sentence(text):
+def tokenize_to_sentence(text, match1, match2):
     """Tokenizes to sentences"""
     sent_list = []
     for i in sent_tokenize(text):
-        if re.search(checks_regex.lower(),re.sub("\W+"," ",i.lower())):
+        if re.search(match1.lower(), i.lower()) and re.search(re.sub("(\(|\))","",match2.lower()), i.lower()) :
             sent_list.append(i)
     return sent_list
 
 # Add sentences to dataframe
-df1["Sentences"] = df1.apply(lambda x:tokenize_to_sentence(x[2]), axis=1)
+df1["Sentences"] = df1.apply(lambda x:tokenize_to_sentence(x[0], x[2], x[3]), axis=1)
 df2 = df1.apply(lambda x: pd.Series(x['Sentences']),axis=1).stack().reset_index(level=1, drop=True)
 df2.name = "Sentence"
-df1  = df1.drop(['Text', 'Sentences'], axis=1).join(df2)
+df1  = df1.drop(['Message', 'Sentences'], axis=1).join(df2)
+df1 = df1[df1["Sentence"].notnull()]
 
 # Write to file
 df1.to_csv("out.csv", sep="\t")
